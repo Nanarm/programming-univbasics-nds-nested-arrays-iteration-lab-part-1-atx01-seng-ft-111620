975197b5cf8e66d600fe798b33c3deb1345224f1
Nan Armbruster <armbruster.nan@gmail.com>
2020-09-19 21:47:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,7 @@ def find_even_values(src)
   play = 0
   array_of_arrays = [ [10, 11], [99, 50, 3, 4], [23, 41] ]
   while array_of_arrays.each do |x| 
-    if x % 2 == 0 
+    if array_of_arrays.even 
     play += 1 
     end
   end
